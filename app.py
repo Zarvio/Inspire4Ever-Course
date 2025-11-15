60b456a2240dcc95a791effc3c9b3a4620c8270c
Zarvio <ds0005217@gmail.com>
2025-11-15 22:58:44 +0530

    Use environment variable for Flask port
    Updated the Flask app to use environment variable for port.

--- app.py
+++ app.py
@@ -157,4 +157,8 @@ def follow_all_users():
 # Run Flask
 # -----------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
